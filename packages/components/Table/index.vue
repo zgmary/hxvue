7023f4b251d55e66869a6141268a3e78d7a5d2f3
hx <zgmary@163.com>
2020-10-08 12:03:39 +0800

    2020.10.08 feat(Table)完成了Table组件及测试Html

--- packages/components/Table/index.vue
+++ packages/components/Table/index.vue
@@ -1,121 +1,64 @@
 <template>
   <div :class="bemCss()">
-    <!-- 顶部按钮 -->
-    <div>
-      <el-button
-        type="success"
-        icon="el-icon-circle-plus-outline"
-        @click="handleAdd"
-      >
-        增加
-      </el-button>
-      <el-button type="warning" icon="el-icon-edit" @click="handleEdit">
-        编辑
-      </el-button>
-      <el-button type="danger" icon="el-icon-delete" @click="handleDel">
-        删除
-      </el-button>
-      <div style="float: right;">
-        <el-input
-          v-model="searchKey"
-          placeholder="请输入性别"
-          style="width: 200px;"
-        />
-        <el-button type="primary" icon="el-icon-search" @click="handleSearch">
-          查询
-        </el-button>
-      </div>
-    </div>
-
-    <div style="height:12px"></div>
-
     <!-- 表格部分 -->
     <el-table
-      :data="data"
-      style="width: 100%"
+      :data="pageData"
+      :size="controlSize"
+      :style="tableStyle"
+      :stripe="option.stripe"
+      :fit="option.fit"
+      :height="option.tableHeight"
       border
-      v-loading="isLoading"
-      element-loading-text="加载中"
+      v-loading="dataLoading"
+      element-loading-background="rgba(0, 0, 0, 0.5)"
+      element-loading-text="数据正在加载中"
+      element-loading-spinner="el-icon-loading"
       @selection-change="handleSelectionChange"
     >
       <!--增加索引-->
-      <el-table-column type="index" min-width="1" align="center">
+      <el-table-column
+        label="序号"
+        type="index"
+        fixed="left"
+        min-width="20"
+        align="center"
+      >
       </el-table-column>
       <!--选择框-->
-      <el-table-column type="selection" min-width="1" align="center">
+      <el-table-column
+        type="selection"
+        fixed="left"
+        min-width="20"
+        align="center"
+      >
       </el-table-column>
       <!--数据列-->
-      <template v-for="(column, index) in columns">
-        <el-table-column
-          :prop="column.prop"
-          :label="column.label"
-          :align="column.align"
-          min-width="3"
-        >
-        </el-table-column>
-      </template>
+      <el-table-column
+        v-for="(column, index) in columns"
+        :key="index"
+        :sortable="column.sortable"
+        :prop="column.prop"
+        :label="column.label"
+        :align="column.align || 'center'"
+        :width="column.width"
+        :formatter="column.formatter"
+        min-width="50"
+      >
+      </el-table-column>
     </el-table>
-
-    <div style="height:20px"></div>
 
     <!-- 分页 -->
     <el-pagination
       @size-change="handleSizeChange"
       @current-change="handleCurrentChange"
-      :current-page="currentPage"
+      :current-page="pagination.currentPage"
       :page-size="pagination.pageSize"
       :page-sizes="pagination.pageSizes"
       :total="tableData.length"
+      background
       layout="total, sizes, prev, pager, next, jumper"
     >
     </el-pagination>
-
-    <!-- 表单部分 -->
-    <el-dialog :title="textMap[dialogStatus]" :visible.sync="dialogFormVisible">
-      <el-form
-        ref="dataForm"
-        :rules="rules"
-        :model="temp"
-        label-position="left"
-        label-width="80px"
-        style="width: 400px; margin-left:50px;"
-      >
-        <el-form-item label="员工账号" prop="id">
-          <el-input v-model="temp.id" />
-        </el-form-item>
-        <el-form-item label="姓名" prop="name">
-          <el-input v-model="temp.name" />
-        </el-form-item>
-        <el-form-item label="年龄" prop="age">
-          <el-input v-model="temp.age" />
-        </el-form-item>
-        <el-form-item label="出生日期" prop="birthDate">
-          <el-date-picker
-            v-model="temp.birthDate"
-            type="date"
-            placeholder="请选择日期"
-            value-format="yyyy-MM-dd"
-          />
-        </el-form-item>
-        <el-form-item label="性别" prop="sex">
-          <el-input v-model="temp.sex" />
-        </el-form-item>
-        <el-form-item label="城市" prop="city">
-          <el-input v-model="temp.city" />
-        </el-form-item>
-      </el-form>
-      <div slot="footer">
-        <el-button @click="dialogFormVisible = false">
-          取消
-        </el-button>
-        <el-button
-          type="primary"
-          @click="dialogStatus === 'create' ? createData() : updateData()"
-        >
-          确定
-        </el-button>
-      </div>
-    </el-dialog>
   </div>
 </template>
 
@@ -140,186 +83,53 @@ export default create({
       type: Object,
       default() {
         return {
-          pageSize: 10,
-          pageSizes: [10, 30, 50, 100]
+          pageSize: 20,
+          pageSizes: [20, 40, 60, 80, 100],
+          currentPage: 1
         };
+      }
+    },
+    option: {
+      type: Object,
+      default() {
+        return {};
       }
     }
   },
-  data() {
-    return {
-      multipleSelection: [],
-      currentPage: 1,
-      isLoading: false,
-      temp: {
-        id: "",
-        name: "",
-        age: "",
-        birthDate: "",
-        sex: "",
-        city: ""
-      },
-      dialogFormVisible: false,
-      dialogStatus: "",
-      searchKey: "",
-      textMap: {
-        update: "修改",
-        create: "添加"
-      },
-      rules: {
-        id: [
-          {
-            required: true,
-            message: "id is required",
-            trigger: "blur"
-          }
-        ],
-        name: [
-          {
-            required: true,
-            message: "name is required",
-            trigger: "blur"
-          }
-        ],
-        birthDate: [
-          {
-            required: true,
-            message: "birthDate is required",
-            trigger: "change"
-          }
-        ]
-      }
-    };
-  },
   computed: {
-    data() {
+    pageData() {
       return this.tableData.slice(
-        (this.currentPage - 1) * this.pagination.pageSize,
-        this.currentPage * this.pagination.pageSize
+        (this.pagination.currentPage - 1) * this.pagination.pageSize,
+        this.pagination.currentPage * this.pagination.pageSize
       );
+    },
+    controlSize() {
+      return this.option.controlSize || "small";
+    },
+    tableStyle() {
+      return this.option.tableStyle || "width: 100%";
+    },
+    dataLoading() {
+      return this.option.dataLoading || false;
     }
   },
   methods: {
     handleSelectionChange(val) {
-      this.multipleSelection = val;
-      console.log("val:", val);
+      this.$emit("selection-change", val);
     },
     // 分页方法
     handleSizeChange(val) {
-      console.log(`每页 ${val} 条`);
-      this.isLoading = true;
-      this.currentPage = 1;
-      setTimeout(() => {
-        this.$emit("changePageSize", val);
-        this.isLoading = false;
-      }, 400);
+      this.pagination.pageSize = val;
     },
     handleCurrentChange(val) {
-      console.log(`当前页: ${val}`);
-      this.isLoading = true;
-      setTimeout(() => {
-        this.currentPage = val;
-        this.isLoading = false;
-      }, 400);
-    },
-
-    resetTemp() {
-      this.temp = {
-        id: "",
-        name: "",
-        age: "",
-        birthDate: "",
-        sex: "",
-        city: ""
-      };
-    },
-    handleAdd() {
-      this.resetTemp();
-      this.dialogStatus = "create";
-      this.dialogFormVisible = true;
-      this.$nextTick(() => {
-        this.$refs["dataForm"].clearValidate();
-      });
-    },
-    createData() {
-      this.$refs["dataForm"].validate(valid => {
-        if (valid) {
-          this.tableData.push(this.temp);
-          this.currentPage = Math.ceil(
-            this.tableData.length / this.pagination.pageSize
-          );
-          this.dialogFormVisible = false;
-          this.$notify({
-            title: "成功",
-            message: "添加成功",
-            type: "success",
-            duration: 2000
-          });
-        }
-      });
-    },
-    handleEdit() {
-      if (this.multipleSelection.length == 1) {
-        this.temp = this.multipleSelection[0];
-        this.dialogStatus = "update";
-        this.dialogFormVisible = true;
-        this.$nextTick(() => {
-          this.$refs["dataForm"].clearValidate();
-        });
-      }
-    },
-    updateData() {
-      this.$refs["dataForm"].validate(valid => {
-        if (valid) {
-          let index = this.tableData.findIndex(
-            item => item.id === this.temp.id
-          );
-          this.tableData.splice(index, 1, this.temp);
-          this.dialogFormVisible = false;
-          this.$notify({
-            title: "成功",
-            message: "修改成功",
-            type: "success",
-            duration: 2000
-          });
-        }
-      });
-    },
-    handleDel() {
-      let dataLen = this.multipleSelection.length;
-      if (dataLen) {
-        for (let i = 0; i < dataLen; i++) {
-          let index = this.tableData.findIndex(
-            item => item.id === this.multipleSelection[i].id
-          );
-          this.tableData.splice(index, 1);
-        }
-        if (this.tableData.length % this.pagination.pageSize == 0) {
-          this.currentPage = this.tableData.length / this.pagination.pageSize;
-        }
-        this.$notify({
-          title: "成功",
-          message: "删除成功",
-          type: "success",
-          duration: 2000
-        });
-      }
-    },
-    handleSearch() {
-      let tempArr = this.tableData.filter(item => item.sex == this.searchKey);
-      this.isLoading = true;
-      setTimeout(() => {
-        this.$emit("handleSearch", tempArr);
-        this.currentPage = 1;
-        this.isLoading = false;
-      }, 400);
+      this.pagination.currentPage = val;
     }
   }
 });
 </script>
 
-<style>
-.el-table th.gutter {
+<style scoped lang="scss">
+>>> .el-table th.gutter {
   /* 解决element-ui 表格篡位的问题 */
   display: table-cell !important;
 }
